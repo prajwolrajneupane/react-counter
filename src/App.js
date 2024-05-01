c10bc97166680c7ec70a9a2f80d5061cf65ddbc9
prajwol <prajwolr88@gmail.com>
2024-05-01 15:46:26 +0545

    just a counter

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './App.css';
+function App() {
+
+  const [value,changeValue]=useState(0);
+
+
+  function increment(){
+changeValue(value+1)
+  }
+
+  function decrement(){
+    changeValue(value-1)
+
+  }
+  return (
+    <div>
+    <p>counter:</p>
+    <span><button onClick={increment}>+</button></span>
+    <span >{value}</span>
+    <span><button onClick={decrement}>-</button></span>
+
+    </div>
+  )
+}
+
+export default App
